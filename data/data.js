003db3fb6deb0d1e34e1acbab279241254af0133
Klaas Nienhuis <mail@klaasnienhuis.nl>
2021-05-07 11:49:23 +0200

    add data file with visibilitygroups. This helps to manage showing and hiding of mutually exclusive options

---
+++ data/data.js
@@ -0,0 +1,102 @@
+var visibilitygroups = {
+  vertebrae: {
+    title: 'Vertebrae',
+    options: [
+      {
+        name: 'hide',
+        objectnames: [
+        ]
+      },
+      {
+        name: 'all',
+        objectnames: [
+          'C1_oud_2',
+          'C1_nieuw_3',
+          'C1_nieuw.001_4',
+          'C1_nieuw.002_5',
+          'C1_nieuw.003_6',
+          'C1_nieuw.004_7',
+          'C1_nieuw.005_8',
+          'C1_nieuw.006_9',
+          'C1_nieuw.007_10',
+          'C1_nieuw.008_11',
+          'C1_nieuw.009_12',
+          'C1_nieuw.010_13',
+        ]
+      },
+      {
+        name: 'a0',
+        objectnames: [
+          'C1_oud_2',
+          'C1_nieuw_3'
+        ]
+      },
+      {
+        name: 'a1',
+        objectnames: [
+          'C1_nieuw_3'
+        ]
+      },
+      {
+        name: 'a2',
+        objectnames: [
+          'C1_nieuw.001_4',
+        ]
+      },
+      {
+        name: 'a3',
+        objectnames: [
+          'C1_nieuw.002_5'
+        ]
+      },
+      {
+        name: 'a4',
+        objectnames: [
+          'C1_nieuw.003_6'
+        ]
+      },
+      {
+        name: 'a5',
+        objectnames: [
+          'C1_nieuw.004_7',
+        ]
+      },
+      {
+        name: 'a6',
+        objectnames: [
+          'C1_nieuw.005_8',
+        ]
+      },
+      {
+        name: 'a7',
+        objectnames: [
+          'C1_nieuw.006_9',
+        ]
+      },
+      {
+        name: 'a8',
+        objectnames: [
+          'C1_nieuw.007_10',
+        ]
+      },
+      {
+        name: 'a9',
+        objectnames: [
+          'C1_nieuw.008_11',
+        ]
+      },
+      {
+        name: 'a10',
+        objectnames: [
+          'C1_nieuw.009_12',
+        ]
+      },
+      {
+        name: 'a11',
+        objectnames: [
+          'C1_nieuw.010_13',
+        ]
+      },
+    ]
+  }
+}
